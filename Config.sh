a7fe83d9480be4cb4e09450420f8ca391bbe0c1c
yangxins <yangmame@icloud.com>
2017-07-10 15:35:08 +0800

    更新内核配置 修复了一些bug 优化说明â

--- Config.sh
+++ Config.sh
@@ -6,11 +6,12 @@ source /etc/profile
 emerge-webrsync
 emerge --sync
 eselect news read
-eselect profile list
-read -p "Input the num you want to use (You must select the systemd !!!
-If you have many software are GTK and you want to use gnome or other desktop which is using GTK , select gnome/systemd
-If you want to use KDE desktop or you have many software are using qt select plasma/systemd
-Just only select /systemd is not a good idea , if you're not sure please select gnome/systemd" TMP
+eselect profile list | grep systemd
+read -p "Input the number you want to use (You must select the systemd !!!
+If you want to use gnome or other desktop which is using GTK , select gnome/systemd
+If you want to use KDE desktop select plasma/systemd
+Just only select /systemd is not a good idea , if you're not sure please select gnome/systemd
+" TMP
 eselect profile set $TMP
 read -p "ENTER to update the system"
 emerge -uvDN @world
@@ -32,7 +33,10 @@ then echo "sys-kernel/gentoo-sources ~amd64" > /etc/portage/package.accept_keywo
 fi
 emerge gentoo-sources genkernel
 while (($TMP!=1&&$TMP!=2&&$TMP!=3));do
-read -p "You should select the systemd in kernel config like this Gentoo Linux --->Support for init systems....managers --->[*] systemd ENTER to continue"
+read -p "You should select the systemd in kernel config like this：
+Gentoo Linux--->
+    Support for init systems....managers --->
+        [*] systemd ENTER to continue"
 read -p "Which way you want to  compile
 [1]  Use Ubuntu kernel config (If you are a new user try this)
 [2]  Use genkernel all
@@ -42,7 +46,7 @@ if (($TMP==1))
 then wget https://raw.githubusercontent.com/yangxins/Gentoo-Installer/master/Kernel-Config/Ubuntu.config
 mv Ubuntu.config /usr/src/linux/.config
 cd /usr/src/linux
-read -p	"Are you using btrfs  filesystem (y or Enter " tmp ##在这里你可以修改成你使用的文件系统
+read -p	"Are you using btrfs filesystem (y or Enter " tmp ##在这里你可以修改成你使用的文件系统
 if [ "$tmp" == y ]
 then emerge btrfs-progs
 fi
@@ -71,9 +75,6 @@ emerge  sys-kernel/linux-firmware
 
 ##NetWork
 read -p  "Install the networkmanager  : If it ask you to change the config file just agree it "
-emerge -a networkmanager
-read -p "Then it will ask you how to chnage the config file  , just input \"-3\" to write it"
-etc-update
 emerge networkmanager
 systemctl enable NetworkManager
 read -p "Input your hostname  " TMP
@@ -99,5 +100,5 @@ grub-install --target=i386-pc $GRUB
 fi
 ln -sf /proc/self/mounts /etc/mtab
 systemd-machine-id-setup
-sed -i 's/## GRUB_CMDLINE_LINUX=\"init=/usr/lib/systemd/systemd\"/GRUB_CMDLINE_LINUX=\"init=/usr/lib/systemd/systemd\"/g' /etc/default/grub
+echo 'GRUB_CMDLINE_LINUX=\"init=/usr/lib/systemd/systemd\"' >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
