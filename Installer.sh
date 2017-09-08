b0ab31944d1949e04dc9a45b00d3c59fc804cbd6
YangMame <yangmame@icloud.com>
2017-09-08 13:03:41 +0800

    Rewriting

--- Installer.sh
+++ Installer.sh
@@ -1,67 +1,85 @@
 #!/bin/bash
 
 ##分区
-rm -r /mnt/gentoo
-mkdir /mnt/gentoo
-read -p "Do you want to adjust the partition ? (Input y to use fdisk or Enter to continue:  " TMP
+umount /mnt/gentoo > /dev/null
+rm -r /mnt/gentoo > /dev/null
+mkdir -v /mnt/gentoo
+
+read -p "是否进行分区？（输入y使用cfdisk进行分区，回车跳过） " TMP
 if [ "$TMP" == y ]
 then fdisk -l
-    read -p "Which disk do you want to adjust ? (/dev/sdX:  " DISK
-    fdisk $DISK
+read -p "输入你想进行分区的磁盘/dev/sdX： " TMP
+cfdisk $TMP
 fi
+
 fdisk -l
-read -p "Input the / mount point:  " ROOT
-read -p "Format it ? (y or Enter  " TMP
+read -p "输入根目录挂载点：  " ROOT
+read -p "是否格式化？ （y或回车）  " TMP
 if [ "$TMP" == y ]
-then read -p "Input y to use ext4 defalut to use btrfs  " TMP
-    if [ "$TMP" == y ]
+then read -p "输入你想使用的文件系统，回车或输入了不支持的文件系统将格式化为ext4（小写字母，请事先确保当前系统支持此文件系统）  " FILESYSTEM
+    if [ "$FILESYSTEM" ==  ]
     then mkfs.ext4 $ROOT
-    else mkfs.btrfs $ROOT -f
+    else if [ "$FILESYSTEM" == btrfs ]
+    then mkfs.btrfs $ROOT -f
+    else if [ "$FILESYSTEM" == xfs ]
+    then mkfs.xfs $ROOT
+    else if [ "$FILESYSTEM" == jfs ]
+    then mkfs.jfs $ROOT
+    else mkfs.ext4 $ROOT
     fi
 fi
-umount $ROOT
-mount $ROOT /mnt/gentoo
-read -p "Do you have the /boot mount point? (y or Enter  " BOOT
+umount $ROOT > /dev/null
+mount -v $ROOT /mnt/gentoo
+
+read -p "是否有Boot分区（UEFI必选，输入efi分区即可）y或回车  " BOOT
 if [ "$BOOT" == y ]
 then fdisk -l
-    read -p "Input the /boot mount point:  " boot
-    read -p "Format it ? (y or Enter  " TMP
+    read -p "输入挂载点:  " boot
+    read -p "是否格式化? (y或回车  " TMP
     if [ "$TMP" == y ]
     then mkfs.fat -F32 $boot
     fi
 fi
-read -p "Do you have the swap partition ? (y or Enter  " SWAP
+
+read -p "是否有交换空间？Gentoo强烈建议使用 (y或回车  " SWAP
 if [ "$SWAP" == y ]
 then fdisk -l
-    read -p "Input the swap mount point:  " SWAP
-    read -P "Format it ? (y or Enter  " TMP
+    read -p "输入挂载点:  " SWAP
+    read -P "是否格式化 ? (y or Enter  " TMP
     if [ "$TMP" == y ]
-    then mkswap $SWAP
+    then mkswap $SWAP > /dev/null
     fi
-    swapon $SWAP
+    swapon $SWAP > /dev/null
 fi
 
 ##安装文件
+read -p "输入y使用openRC 回车使用systemd " INIT
 cd /mnt/gentoo
-wget -c -r -np -k -L -p http://mirrors.ustc.edu.cn/gentoo/releases/amd64/autobuilds/current-stage3-amd64-systemd/
-cp mirrors.ustc.edu.cn/gentoo/releases/amd64/autobuilds/current-stage3-amd64-systemd/stage3-amd64-systemd*.tar.bz2 install.tar.bz2
-tar xvjpf install.tar.bz2 --xattrs --numeric-owner
-rm -r mirrors.ustc.edu.cn/ install.tar.bz2
+rm index.html > /dev/null
+if [ "$INIT" == y ]
+then LATEST=$(wget -q http://mirrors.ustc.edu.cn/gentoo/releases/amd64/autobuilds/current-stage3-amd64/ && grep -o stage3-amd64-.........tar.bz2 index.html | head -1)
+wget http://mirrors.ustc.edu.cn/gentoo/releases/amd64/autobuilds/current-stage3-amd64/$LATEST
+tar xvjpf $LATEST --xattrs --numeric-owner
+else 
+LATEST=$(wget -q http://mirrors.ustc.edu.cn/gentoo/releases/amd64/autobuilds/current-stage3-amd64-systemd/ && grep -o stage3-amd64-systemd-.........tar.bz2 index.html | head -1)
+wget http://mirrors.ustc.edu.cn/gentoo/releases/amd64/autobuilds/current-stage3-amd64-systemd/$LATEST
+tar xvjpf $LATEST --xattrs --numeric-owner
+fi
 
 if [ "$BOOT" == y ]
-then umount $boot
-     mount $boot /mnt/gentoo/boot
+then umount $boot > /dev/null
+     mount -v $boot /mnt/gentoo/boot
 fi
 
 ##配置make.conf
-sed -i 's/CFLAGS=\"-O2 -pipe\"/CFLAGS=\"-march=native -O2 -pipe\"/g' /mnt/gentoo/etc/portage/make.conf ##你可以在此根据你的CPU修改优化例如改成-march=haswell -O3 -pipe（主要是因为我懒 后期会加上）
+sed -i 's/CFLAGS=\"-O2 -pipe\"/CFLAGS=\"-march=native -O2 -pipe\"/g' /mnt/gentoo/etc/portage/make.conf ##你可以在此根据你的CPU修改优化例如改成-march=haswell -O3 -pipe
 echo "GENTOO_MIRRORS=\"https://mirrors.ustc.edu.cn/gentoo/\" ">> /mnt/gentoo/etc/portage/make.conf ##如果此软件源巨慢或者你在国外 可以自行修改
 echo "L10N=\"en-US zh-CN\"
 LINGUAS=\"en_US zh_CN\"" >> /mnt/gentoo/etc/portage/make.conf
-read -p "Edit the make.conf if you want to add your video cards like VIDEO_CARDS=\"intel\" VIDEO_CARDS=\"nvidia\" or VIDEO_CARDS=\"intel i965 nvidia\"(y or ENTER" TMP ##懒得加选择  其实无所谓 可以自己手动安装
-if [ "$TMP" == y ]
-then nano  /mnt/gentoo/etc/portage/make.conf
-fi
+
+##Video Cards
+
+
 mkdir /mnt/gentoo/etc/portage/repos.conf
 echo "[DEFAULT]
 main-repo = gentoo
@@ -72,7 +90,7 @@ sync-type = rsync
 sync-uri = rsync://rsync.mirrors.ustc.edu.cn/gentoo-portage/
 auto-sync = yes" >> /mnt/gentoo/etc/portage/repos.conf/gentoo.conf ##同上上
 
-##chroot
+##Chroot
 mount -t proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
@@ -83,12 +101,4 @@ cd root/
 wget https://raw.githubusercontent.com/YangMame/Gentoo-Installer/master/Config.sh
 chmod +x Config.sh
 rm /mnt/gentoo/etc/fstab
-genfstab -U -p /mnt/gentoo >> /mnt/gentoo/etc/fstab
-read -p "It will edit the fstab and you should make sure this file is all right
-you need to del something like efivarfs or cgroup
-If you found this file is empty without the right mount point
-Then you should open another terminal and run \"sudo blkid\"
-Write like this : UUID=XXX....  /   ext4    defaults    0 0
-ENTER to contiune "
-nano /mnt/gentoo/etc/fstab
 chroot /mnt/gentoo /root/Config.sh
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-## 已可以正常使用 欢迎提需求
+## 重写ing 请勿使用
 
- Live系统推荐用Arch linux的live CD 因为有genfstab（其它内置genfstab或者安装此程序的也可以使用）
+ Live系统推荐用Ubuntu 
 
- 仅支持amd64系统 使用systemd 支持UEFI 三种内核配置：Ubuntu内核配置 Genkernel all 还有从网络下载配置或全手动配置（之所以不用Arch的内核配置是因为驱动可能不如Ubuntu全面）
-
- (如果你的efi分区是直接挂载到/boot而不是/boot/efi 可能造成grub混乱 建议：把现有系统的引导换成bootctl：https://wiki.archlinux.org/index.php/Systemd-boot)
-
-
+ #支持：
+Init：openRC,systemd
+Arch:x86_64
+Boot:Legacy,UEFI
+Kernel:Genkernel,Download From Internet,Config By Yourself
 
 下载地址：https://raw.githubusercontent.com/YangMame/Gentoo-Installer/master/Installer.sh
