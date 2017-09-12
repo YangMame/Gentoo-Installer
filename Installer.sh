2b1d3e09642ef7e6c031491f33f318b88d04aa6a
YangMame <yangmame@icloud.com>
2017-09-12 11:27:14 +0800

    完成第一部分

--- Installer.sh
+++ Installer.sh
@@ -18,14 +18,14 @@ fdisk -l
 read -p "输入根目录挂载点：  " ROOT
 read -p "是否格式化？ （y或回车）  " TMP
 if [ "$TMP" == y ]
-then read -p "输入你想使用的文件系统，回车或输入了不支持的文件系统将格式化为ext4（小写字母，请事先确保当前系统支持此文件系统）  " FILESYSTEM
+then read -p "输入你想使用的文件系统:btrfs,xfs,jfs 回车或者其他字符将默认格式化为ext4(请事先确认当前系统支持此文件系统且有相应工具) " FILESYSTEM
     if [ "$FILESYSTEM" ==  ]
     then mkfs.ext4 $ROOT
-    else if [ "$FILESYSTEM" == btrfs ]
+    elif [ "$FILESYSTEM" == btrfs ]
     then mkfs.btrfs $ROOT -f
-    else if [ "$FILESYSTEM" == xfs ]
+    elif [ "$FILESYSTEM" == xfs ]
     then mkfs.xfs $ROOT
-    else if [ "$FILESYSTEM" == jfs ]
+    elif [ "$FILESYSTEM" == jfs ]
     then mkfs.jfs $ROOT
     else mkfs.ext4 $ROOT
     fi
@@ -61,12 +61,16 @@ rm index.html > /dev/null
 if [ "$INIT" == y ]
 then LATEST=$(wget -q $STAGE_MIRRORS/current-stage3-amd64/ && grep -o stage3-amd64-.........tar.bz2 index.html | head -1)
 wget $STAGE_MIRRORS/current-stage3-amd64/$LATEST
-tar xvjpf $LATEST --xattrs --numeric-owner
+echo 解压中...
+tar xjpf $LATEST --xattrs --numeric-owner
 else 
 LATEST=$(wget -q $STAGE_MIRRORS/current-stage3-amd64-systemd/ && grep -o stage3-amd64-systemd-.........tar.bz2 index.html | head -1)
 wget $STAGE_MIRRORS/current-stage3-amd64-systemd/$LATEST
-tar xvjpf $LATEST --xattrs --numeric-owner
+echo 解压中...
+tar xjpf $LATEST --xattrs --numeric-owner
 fi
+
+rm $LATEST
 
 if [ "$BOOT" == y ]
 then umount $boot > /dev/null
@@ -80,7 +84,28 @@ echo "L10N=\"en-US zh-CN\"
 LINGUAS=\"en_US zh_CN\"" >> /mnt/gentoo/etc/portage/make.conf
 
 ##Video Cards
-
+VIDEO=6
+while (($VIDEO!=1&&$VIDEO!=2&&$VIDEO!=3&&$VIDEO!=4&&$VIDEO!=5));do
+echo "输入对应的显卡配置
+[1]  Intel
+[2]  Nvidia
+[3]  Intel/Nvidia
+[4]  AMD/ATI
+[5]  Intel/AMD"
+read VIDEO
+if [ "$VIDEO" == 1 ]
+then echo VIDEO_CARDS=\"intel i965\" >> /mnt/gentoo/etc/portage/make.conf
+elif [ "$VIDEO" == 2 ]
+then echo VIDEO_CARDS=\"nvidia\" >> /mnt/gentoo/etc/portage/make.conf
+elif [ "$VIDEO" == 3 ]
+then echo VIDEO_CARDS=\"intel i965 nvidia\" >> /mnt/gentoo/etc/portage/make.conf
+elif [ "$VIDEO" == 4 ]
+then echo VIDEO_CARDS=\"radeon\" >> /mnt/gentoo/etc/portage/make.conf
+elif [ "$VIDEO" == 5 ]
+then echo VIDEO_CARDS=\"intel i965 radeon\" >> /mnt/gentoo/etc/portage/make.conf
+else echo 请输入正确数字
+fi
+done
 
 mkdir /mnt/gentoo/etc/portage/repos.conf
 echo "[DEFAULT]
@@ -90,7 +115,7 @@ main-repo = gentoo
 location = /usr/portage
 sync-type = rsync
 sync-uri = rsync://rsync.mirrors.ustc.edu.cn/gentoo-portage/
-auto-sync = yes" >> /mnt/gentoo/etc/portage/repos.conf/gentoo.conf ##同上上
+auto-sync = yes" > /mnt/gentoo/etc/portage/repos.conf/gentoo.conf ##同上上
 
 ##Chroot
 mount -t proc /proc /mnt/gentoo/proc
@@ -98,6 +123,7 @@ mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
+rm /mnt/gentoo/etc/resolv.conf > /dev/null
 cp /etc/resolv.conf /mnt/gentoo/etc/
 cd root/
 wget https://raw.githubusercontent.com/YangMame/Gentoo-Installer/master/Config.sh
