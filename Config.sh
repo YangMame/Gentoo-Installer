759305c49debadfc6295d355006e77a286b599f5
YangMame <yangmame@icloud.com>
2017-09-14 11:13:16 +0800

    完善和美化脚本，可能会有语法错误

--- Config.sh
+++ Config.sh
@@ -35,51 +35,60 @@ rm -r arch-install-scripts-17*
 
 ##内核
 read -p "使用哪个内核？ck-sources(推荐)或gentoo-sources输入y使用ck回车将使用gentoo-sources  " TMP
-if [ "$TMP" == y ]
-then echo sys-kernel/ck-sources ~amd64 >> /etc/portage/package.accept_keywords
-emerge ck-sources genkernel
-else emerge gentoo-sources genkernel
+if [ "$TMP" == y ];then
+	echo sys-kernel/ck-sources ~amd64 >> /etc/portage/package.accept_keywords
+	emerge ck-sources genkernel
+else
+	emerge gentoo-sources genkernel
 fi
 
 ##安装文件系统工具
-if [ $1 == btrfs ]
-then emerge sys-fs/btrfs-progs
-elif [ $1 == xfs ]
-then emerge sys-fs/xfsprogs
-elif [ $1 == jfs ]
-then emerge sys-fs/jfsutils
+if [ $1 == btrfs ];then
+	emerge sys-fs/btrfs-progs
+elif [ $1 == xfs ];then
+	emerge sys-fs/xfsprogs
+elif [ $1 == jfs ];then
+	emerge sys-fs/jfsutils
 fi
+
+##编译安装内核
 
 cd /usr/src/linux
 
 TMP=4
 while (($TMP!=1&&$TMP!=2&&$TMP!=3));do
-    read -p "你想如何编译内核
+read -p "你想如何编译内核
 [1]  从网络上下载自己的内核配置
 [2]  使用genkernel all （如果你不会配置内核 建议选择这个）
 [3]  现场开始编译（建议参照金步国的文档配置：http://www.jinbuguo.com/kernel/longterm-linux-kernel-options.html）
 : " TMP
-    if (($TMP==1))
-    then read -p "输入下载地址：" LINK
-	wget -v $LINK -O .config
-        make menuconfig
-	read -p "回车开始编译安装内核"
-        make -j8 && make modules_install ##根据你CPU修改-j8 推荐核数x2
-        make install
-        genkernel --install initramfs
-    elif (($TMP==2))
-    then genkernel all
-    elif (($TMP==3))
-    then
-	rm .config > /dev/null
-        make menuconfig
-	read -p "回车开始编译安装内核"
-        make -j8 && make modules_install
-        make install
-        genkernel --install initramfs
-    else echo 请输入正确数字
-    fi
+	if [ "$TMP" == 1 ];then
+		tmp=n
+		while [ "$tmp" == n ];do
+			read -p "输入下载地址：" LINK
+			wget -v $LINK -O .config
+			make menuconfig
+			read -p "回车开始编译安装内核(输入n重新下载)" tmp
+		done
+		make -j8 && make modules_install ##根据你CPU修改-j8 推荐核数x2
+		make install
+		genkernel --install initramfs
+	elif [ "$TMP" == 2 ];then
+		genkernel all
+	elif [ "$TMP" == 3 ];then
+		rm .config > /dev/null
+		tmp=n
+		while [ "$tmp" == n ];do
+			make menuconfig
+			read -p "回车开始编译安装内核(输入n重新进入配置)" tmp
+		done
+		make -j8 && make modules_install
+		make install
+		genkernel --install initramfs
+	else echo 请输入正确数字
+	fi
 done
+
 emerge  sys-kernel/linux-firmware
 
 ##NetWork
@@ -93,28 +102,32 @@ echo hostname=\"$TMP\" > /etc/conf.d/hostname
 ##Tools
 emerge app-admin/sysklogd
 emerge sys-process/cronie
+emerge sudo
+sed -i 's/\# \%wheel ALL=(ALL) ALL/\%wheel ALL=(ALL) ALL/g' /etc/sudoers
 crontab /etc/crontab
 echo "更改root密码 "
 passwd
 
 ##GRUB
 read -p "是否是UEFI启动？输入y或回车为legacy安装grub " TMP
-if [ "$TMP" == y ]
-then echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
-    emerge grub
-    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Gentoo
-else emerge grub
-    fdisk -l
-    read -p "Input the disk you want to install the grub  " GRUB
-    grub-install --target=i386-pc $GRUB
+if [ "$TMP" == y ];then
+	echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
+	emerge grub
+	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Gentoo
+else
+	emerge grub
+	fdisk -l
+	read -p "Input the disk you want to install the grub  " GRUB
+	grub-install --target=i386-pc $GRUB
 fi
 
-if [ $2 == systemd ]
-then sed -i 's/\# GRUB_CMDLINE_LINUX=\"init=\/usr\/lib\/systemd\/systemd\"/GRUB_CMDLINE_LINUX=\"init=\/usr\/lib\/systemd\/systemd\"/g' /etc/default/grub
-ln -sf /proc/self/mounts /etc/mtab
-systemd-machine-id-setup
-systemctl enable NetworkManager
-else rc-update add NetworkManager default
+if [ $2 == systemd ];then
+	sed -i 's/\# GRUB_CMDLINE_LINUX=\"init=\/usr\/lib\/systemd\/systemd\"/GRUB_CMDLINE_LINUX=\"init=\/usr\/lib\/systemd\/systemd\"/g' /etc/default/grub
+	ln -sf /proc/self/mounts /etc/mtab
+	systemd-machine-id-setup
+	systemctl enable NetworkManager
+else
+	rc-update add NetworkManager default
 fi
 
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -126,45 +139,46 @@ echo "设置用户密码"
 passwd $USER
 
 ##安装桌面环境
-if [ "$PROFILE" == 5 ]
-then emerge gnome-shell gdm gnome-termianl
-gpasswd -a $USER gdm
-systemctl enable gdm
-elif [ "$PROFILE" == 6 ]
-then emerge --autounmask-write kde-plasma/plasma-meta sddm konsole plasma-pa plasma-nm
-etc-update --automode -3
-emerge kde-plasma/plasma-meta sddm konsole plasma-pa plasma-nm
-sed -i 's/DISPLAYMANAGER=\"xdm\"/DISPLAYMANAGER=\"sddm\"/g' /etc/conf.d/xdm
-usermod -a -G video sddm
-gpasswd -a $USER sddm
-sddm --example-config > /etc/sddm.conf
-rc-update add xdm default
-elif [ "$PROFILE" == 7 ]
-then emerge --autounmask-write kde-plasma/plasma-meta sddm konsole plasma-pa plasma-nm
-etc-update --automode -3
-emerge kde-plasma/plasma-meta sddm konsole plasma-pa plasma-nm
-systemctl enable sddm
-usermod -a -G video sddm
-gpasswd -a $USER sddm
-sddm --example-config > /etc/sddm.conf
+if [ "$PROFILE" == 5 ];then
+	emerge gnome-shell gdm gnome-termianl
+	gpasswd -a $USER gdm
+	systemctl enable gdm
+elif [ "$PROFILE" == 6 ];then
+	emerge --autounmask-write kde-plasma/plasma-meta sddm konsole plasma-pa plasma-nm
+	etc-update --automode -3
+	emerge kde-plasma/plasma-meta sddm konsole plasma-pa plasma-nm
+	sed -i 's/DISPLAYMANAGER=\"xdm\"/DISPLAYMANAGER=\"sddm\"/g' /etc/conf.d/xdm
+	usermod -a -G video sddm
+	gpasswd -a $USER sddm
+	sddm --example-config > /etc/sddm.conf
+	rc-update add xdm default
+elif [ "$PROFILE" == 7 ];then
+	emerge --autounmask-write kde-plasma/plasma-meta sddm konsole plasma-pa plasma-nm
+	etc-update --automode -3
+	emerge kde-plasma/plasma-meta sddm konsole plasma-pa plasma-nm
+	systemctl enable sddm
+	usermod -a -G video sddm
+	gpasswd -a $USER sddm
+	sddm --example-config > /etc/sddm.conf
 fi
 
 ##Bumblebee双显卡配置
-if [ $3 == 3 ]
-then echo "=sys-power/bbswitch-9999 **
-=x11-misc/bumblebee-9999 **
-=x11-misc/primus-0.2 ~amd64" >> /etc/portage/package.accept_keywords
-emerge --autounmask-write bbswitch primus bumblebee
-etc-update --automode -3
-emerge bbswitch primus bumblebee
-sed -i 's/Bridge=auto/Bridge=primus/g' /etc/bumblebee/bumblebee.conf
-sed -i 's/PMMethod=auto/PMMethod=bbswitch/g' /etc/bumblebee/bumblebee.conf
-sed -i '5,7d' /etc/init.d/bumblebee
-if [ $2 == systemd ]
-then systemctl enable bumblebeed
-else rc-update add bumblebee default
-fi
-gpasswd -a $USER bumblebee
+if [ $3 == 3 ];then
+	echo "=sys-power/bbswitch-9999 **
+	=x11-misc/bumblebee-9999 **
+	=x11-misc/primus-0.2 ~amd64" >> /etc/portage/package.accept_keywords
+	emerge --autounmask-write bbswitch primus bumblebee
+	etc-update --automode -3
+	emerge bbswitch primus bumblebee
+	sed -i 's/Bridge=auto/Bridge=primus/g' /etc/bumblebee/bumblebee.conf
+	sed -i 's/PMMethod=auto/PMMethod=bbswitch/g' /etc/bumblebee/bumblebee.conf
+	sed -i '5,7d' /etc/init.d/bumblebee
+	if [ $2 == systemd ];then
+		systemctl enable bumblebeed
+	else
+		rc-update add bumblebee default
+	fi
+		gpasswd -a $USER bumblebee
 fi
 
 ##自定义
--- Installer.sh
+++ Installer.sh
@@ -5,81 +5,89 @@ STAGE_MIRRORS=http://mirrors.ustc.edu.cn/gentoo/releases/amd64/autobuilds/
 GENTOO_MIRRORS=https://mirrors.ustc.edu.cn/gentoo/
 PORTAGE_MIRRORS=rsync://rsync.mirrors.ustc.edu.cn/gentoo-portage/
 
+##判断用户
+if [ `whoami` != root ];then
+	echo "请在root用户下运行,接下来可能需要你输入密码（liveCD不需要输入）"
+	sudo -i
+fi
+
 ##分区
 umount /mnt/gentoo > /dev/null
 rm -r /mnt/gentoo > /dev/null
 mkdir -v /mnt/gentoo
 
 read -p "是否进行分区？（输入y使用cfdisk进行分区，回车跳过） " TMP
-if [ "$TMP" == y ]
-then fdisk -l
-read -p "输入你想进行分区的磁盘/dev/sdX： " TMP
-cfdisk $TMP
+if [ "$TMP" == y ];then
+	fdisk -l
+	read -p "输入你想进行分区的磁盘/dev/sdX： " TMP
+	cfdisk $TMP
 fi
 
 fdisk -l
 read -p "输入根目录挂载点：  " ROOT
 read -p "是否格式化？ （y或回车）  " TMP
-if [ "$TMP" == y ]
-then read -p "输入你想使用的文件系统:btrfs,xfs,jfs 回车或者其他字符将默认格式化为ext4(请事先确认当前系统支持此文件系统且有相应工具) " FILESYSTEM
-    if [ "$FILESYSTEM" ==  ]
-    then FILESYSTEM=ext4&&mkfs.ext4 $ROOT
-    elif [ "$FILESYSTEM" == btrfs ]
-    then mkfs.btrfs $ROOT -f
-    elif [ "$FILESYSTEM" == xfs ]
-    then mkfs.xfs $ROOT
-    elif [ "$FILESYSTEM" == jfs ]
-    then mkfs.jfs $ROOT
-    else FILESYSTEM=ext4&&mkfs.ext4 $ROOT
-    fi
+if [ "$TMP" == y ];then
+	read -p "输入你想使用的文件系统:btrfs,xfs,jfs 回车或者其他字符将默认格式化为ext4(请事先确认当前系统支持此文件系统且有相应工具) " FILESYSTEM
+	if [ "$FILESYSTEM" ==  ];then
+		FILESYSTEM=ext4&&mkfs.ext4 $ROOT
+	elif [ "$FILESYSTEM" == btrfs ];then
+		mkfs.btrfs $ROOT -f
+	elif [ "$FILESYSTEM" == xfs ];then
+		mkfs.xfs $ROOT
+	elif [ "$FILESYSTEM" == jfs ];then
+		mkfs.jfs $ROOT
+	else
+		FILESYSTEM=ext4&&mkfs.ext4 $ROOT
+	fi
 fi
+
 umount $ROOT > /dev/null
 mount -v $ROOT /mnt/gentoo
 
 read -p "是否有Boot分区（UEFI必选，输入efi分区即可）y或回车  " BOOT
-if [ "$BOOT" == y ]
-then fdisk -l
-    read -p "输入挂载点:  " boot
-    read -p "是否格式化? (y或回车  " TMP
-    if [ "$TMP" == y ]
-    then mkfs.fat -F32 $boot
-    fi
+if [ "$BOOT" == y ];then
+	fdisk -l
+	read -p "输入挂载点:  " boot
+	read -p "是否格式化? (y或回车  " TMP
+	if [ "$TMP" == y ];then
+		mkfs.fat -F32 $boot
+	fi
 fi
 
 read -p "是否有交换空间？Gentoo强烈建议使用 (y或回车  " SWAP
-if [ "$SWAP" == y ]
-then fdisk -l
-    read -p "输入挂载点:  " SWAP
-    read -P "是否格式化 ? (y or Enter  " TMP
-    if [ "$TMP" == y ]
-    then mkswap $SWAP > /dev/null
-    fi
-    swapon $SWAP > /dev/null
+if [ "$SWAP" == y ];then
+	fdisk -l
+	read -p "输入挂载点:  " SWAP
+	read -P "是否格式化 ? (y or Enter  " TMP
+	if [ "$TMP" == y ];then
+		mkswap $SWAP > /dev/null
+	fi
+	swapon $SWAP > /dev/null
 fi
 
 ##安装文件
 read -p "输入y使用openRC 回车使用systemd(如果你使用gnome桌面请务必选择systemd) " TMP
 cd /mnt/gentoo
 rm index.html > /dev/null
-if [ "$INIT" == y ]
-INIT=openrc
-then LATEST=$(wget -q $STAGE_MIRRORS/current-stage3-amd64/ && grep -o stage3-amd64-.........tar.bz2 index.html | head -1)
-wget $STAGE_MIRRORS/current-stage3-amd64/$LATEST
-echo 解压中...
-tar xjpf $LATEST --xattrs --numeric-owner
+if [ "$INIT" == y ];then
+	INIT=openrc
+	LATEST=$(wget -q $STAGE_MIRRORS/current-stage3-amd64/ && grep -o stage3-amd64-.........tar.bz2 index.html | head -1)
+	wget $STAGE_MIRRORS/current-stage3-amd64/$LATEST
+	echo 解压中...
+	tar xjpf $LATEST --xattrs --numeric-owner
 else
-INIT=systemd 
-LATEST=$(wget -q $STAGE_MIRRORS/current-stage3-amd64-systemd/ && grep -o stage3-amd64-systemd-.........tar.bz2 index.html | head -1)
-wget $STAGE_MIRRORS/current-stage3-amd64-systemd/$LATEST
-echo 解压中...
-tar xjpf $LATEST --xattrs --numeric-owner
+	INIT=systemd 
+	LATEST=$(wget -q $STAGE_MIRRORS/current-stage3-amd64-systemd/ && grep -o stage3-amd64-systemd-.........tar.bz2 index.html | head -1)
+	wget $STAGE_MIRRORS/current-stage3-amd64-systemd/$LATEST
+	echo 解压中...
+	tar xjpf $LATEST --xattrs --numeric-owner
 fi
 
 rm $LATEST
 
-if [ "$BOOT" == y ]
-then umount $boot > /dev/null
-     mount -v $boot /mnt/gentoo/boot
+if [ "$BOOT" == y ];then
+	umount $boot > /dev/null
+	mount -v $boot /mnt/gentoo/boot
 fi
 
 ##配置make.conf
@@ -91,24 +99,24 @@ LINGUAS=\"en_US zh_CN\"" >> /mnt/gentoo/etc/portage/make.conf
 ##Video Cards
 VIDEO=6
 while (($VIDEO!=1&&$VIDEO!=2&&$VIDEO!=3&&$VIDEO!=4&&$VIDEO!=5));do
-echo "输入对应的显卡配置
+	echo "输入对应的显卡配置
 [1]  Intel
 [2]  Nvidia
 [3]  Intel/Nvidia
 [4]  AMD/ATI
 [5]  Intel/AMD"
-read VIDEO
-if [ "$VIDEO" == 1 ]
-then echo VIDEO_CARDS=\"intel i965\" >> /mnt/gentoo/etc/portage/make.conf
-elif [ "$VIDEO" == 2 ]
-then echo VIDEO_CARDS=\"nvidia\" >> /mnt/gentoo/etc/portage/make.conf
-elif [ "$VIDEO" == 3 ]
-then echo VIDEO_CARDS=\"intel i965 nvidia\" >> /mnt/gentoo/etc/portage/make.conf
-elif [ "$VIDEO" == 4 ]
-then echo VIDEO_CARDS=\"radeon\" >> /mnt/gentoo/etc/portage/make.conf
-elif [ "$VIDEO" == 5 ]
-then echo VIDEO_CARDS=\"intel i965 radeon\" >> /mnt/gentoo/etc/portage/make.conf
-else echo 请输入正确数字
+	read VIDEO
+	if [ "$VIDEO" == 1 ];then
+		echo VIDEO_CARDS=\"intel i965\" >> /mnt/gentoo/etc/portage/make.conf
+	elif [ "$VIDEO" == 2 ];then
+		echo VIDEO_CARDS=\"nvidia\" >> /mnt/gentoo/etc/portage/make.conf
+	elif [ "$VIDEO" == 3 ];then
+		echo VIDEO_CARDS=\"intel i965 nvidia\" >> /mnt/gentoo/etc/portage/make.conf
+	elif [ "$VIDEO" == 4 ];then
+		echo VIDEO_CARDS=\"radeon\" >> /mnt/gentoo/etc/portage/make.conf
+	elif [ "$VIDEO" == 5 ];then
+		echo VIDEO_CARDS=\"intel i965 radeon\" >> /mnt/gentoo/etc/portage/make.conf
+	else echo 请输入正确数字
 fi
 done
 
